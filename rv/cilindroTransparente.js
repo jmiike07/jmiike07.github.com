14d75179c46bf92578677af791c2243411566d47
jmiike07 <jmiike07@gmail.com>
2016-04-29 13:57:25 -0500

    Create cilindroTransparente.js

---
+++ rv/cilindroTransparente.js
@@ -0,0 +1,51 @@
+function Cilindro(){
+  THREE.Object3D.call(this);
+  this.cilindro=new THREE.Mesh(new THREE.CylinderGeometry(2.5, 2.5, 4.5), new THREE.MeshLambertMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading } ));
+  this.add(this.cilindro)
+  }
+  
+  cilindro.prototype=new THREE.Object3D();
+  
+  function setup(){
+  cilindroT = new cilindro();
+  
+  luzPuntual = new THREE.PointLight(0xffffff);
+  luzPuntual.position.x=10;
+  luzPuntual.position.y=10;
+  luzPuntual.position.z=10;
+
+  escena = new THREE.Scene();
+  escena.add(cilindroT);
+  escena.add(luzPuntual);
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z=20;
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize (window.innerHeight*0.95, window.innerHeight*0.95);
+  document.body.appendChild( renderer.domElement );
+  }
+
+function loop(){
+
+  chkIluminacion=document.getElementById("lighting");
+  
+  
+        if (chkBlending.checked)
+        {
+            cubo.material.transparent = true;
+            cubo.material.opacity = parseFloat(txtBlending.value);
+        }
+        else
+        {
+            cubo.material.transparent = false;
+            cubo.material.opacity = 1.0;
+        }
+        cubo.material.needsUpdate = true;
+
+}
+
+
+var escena,luzPuntual,camara,renderer;
+var cilindroT;
+
+setup();
+loop();
