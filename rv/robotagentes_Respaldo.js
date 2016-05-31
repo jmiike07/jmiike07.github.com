b8940d093939a6d0fce81a27e94f18a3d296c239
jmiike07 <jmiike07@gmail.com>
2016-05-31 05:41:56 -0500

    Update robotagentes_Respaldo.js

--- rv/robotagentes_Respaldo.js
+++ rv/robotagentes_Respaldo.js
@@ -278,21 +278,18 @@ function setup(){
  luzPuntual.position.y=10;
  luzPuntual.position.z=30;
  
- camara1 = new THREE.OrthographicCamera( 31 / -2, 27 / 2, 31/ 2, 29/ - 2, 10, 1000 );
- camara1.position.z=50;
+ camO = new THREE.OrthographicCamera( 31 / -2, 27 / 2, 31/ 2, 29/ - 2, 10, 1000 );
+ camO.position.z=50;
 
- camara2 = new THREE.PerspectiveCamera( );
- camara2.position.z=30;
-  camara2.position.x=10;
- 
-camara2.rotation.y=Math.PI/6;
+ camP = new THREE.PerspectiveCamera( );
+ camP.position.z=10;
 
  
  renderer = new THREE.WebGLRenderer();
  renderer.setSize(window.innerHeight*0.95, window.innerHeight*0.95);
  document.body.appendChild(renderer.domElement);
- entorno.add(camara1);
- entorno.add(camara2);
+ entorno.add(camO);
+ entorno.add(camP);
  entorno.add(luzPuntual);
 
 
@@ -304,12 +301,12 @@ function loop(){
  entorno.sense();
  entorno.plan();
  entorno.act();
-camara2.position.x=px;
- camara2.position.y=py;
+ camP.position.x=px;
+ camP.position.y=py;
  if(keyboard.pressed("P")||keyboard.pressed("p"))
-  renderer.render(entorno,camara2);
+  renderer.render(entorno,camP);
  else  
-  renderer.render(entorno,camara1);
+  renderer.render(entorno,camO);
 
 }
 
